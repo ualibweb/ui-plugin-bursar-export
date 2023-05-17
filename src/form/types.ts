4cd7ab19cd9be55765af17d89bae24b5b1d93bf0
Noah Overcash <novercash@ebsco.com>
2023-05-17 14:43:44 -0500

    Add scheduling section (close #3)

--- src/form/types.ts
+++ src/form/types.ts
@@ -1,1 +1,13 @@
-export interface FormValues {}
+export interface FormValues {
+  scheduling: {
+    frequency: SchedulingFrequency;
+    interval?: number;
+  };
+}
+
+export enum SchedulingFrequency {
+  Manual = 'NONE',
+  Hours = 'HOUR',
+  Days = 'DAY',
+  Weeks = 'WEEK',
+}
