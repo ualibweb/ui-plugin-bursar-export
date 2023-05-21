228fa1e8a2f0c56a66ab3f5551fac69924883559
Noah Overcash <novercash@ebsco.com>
2023-05-21 04:15:18 -0500

    Fully test HeaderFooterCard

--- src/components/Token/HeaderFooter/HeaderFooterCard.test.tsx
+++ src/components/Token/HeaderFooter/HeaderFooterCard.test.tsx
@@ -4,24 +4,54 @@ import React from 'react';
 import { Form } from 'react-final-form';
 import withIntlConfiguration from '../../../test/util/withIntlConfiguration';
 import HeaderFooterCard from './HeaderFooterCard';
+import { HeaderFooterTokenType } from '../../../types/TokenTypes';
 
-test('Header/footer card renders appropriately', async () => {
-  render(
-    withIntlConfiguration(
-      <Form mutators={{ ...arrayMutators }} onSubmit={() => ({})}>
-        {({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <HeaderFooterCard
-              fieldArrayName="test"
-              name="test[0]"
-              index={0}
-              isLast
-            />
-          </form>
-        )}
-      </Form>
-    )
-  );
+describe('Header/footer card', () => {
+  test('has good default value', async () => {
+    render(
+      withIntlConfiguration(
+        <Form mutators={{ ...arrayMutators }} onSubmit={() => ({})}>
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <HeaderFooterCard
+                fieldArrayName="test"
+                name="test[0]"
+                index={0}
+                isLast
+              />
+            </form>
+          )}
+        </Form>
+      )
+    );
 
-  expect(await screen.findByRole('combobox')).toBeVisible();
+    expect(await screen.findByRole('combobox')).toHaveDisplayValue(
+      'Newline (LF)'
+    );
+  });
+
+  it('respects initial value', async () => {
+    render(
+      withIntlConfiguration(
+        <Form
+          mutators={{ ...arrayMutators }}
+          onSubmit={() => ({})}
+          initialValues={{ test: [{ type: HeaderFooterTokenType.COMMA }] }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <HeaderFooterCard
+                fieldArrayName="test"
+                name="test[0]"
+                index={0}
+                isLast
+              />
+            </form>
+          )}
+        </Form>
+      )
+    );
+
+    expect(await screen.findByRole('combobox')).toHaveDisplayValue('Comma');
+  });
 });
