dd31fbd229e0981480b4a6ef37b60158aebed0b8
Noah Overcash <novercash@ebsco.com>
2023-05-22 16:16:49 -0500

    Add transfer types

--- src/types/FormValues.ts
+++ src/types/FormValues.ts
@@ -1,5 +1,6 @@
 import { CriteriaGroup, CriteriaTerminal } from './CriteriaTypes';
 import SchedulingFrequency from './SchedulingFrequency';
+import { DataToken, HeaderFooterToken } from './TokenTypes';
 
 export default interface FormValues {
   scheduling: {
@@ -7,4 +8,18 @@ export default interface FormValues {
     interval?: number;
   };
   criteria: CriteriaGroup | CriteriaTerminal;
+  header?: HeaderFooterToken[];
+  data?: DataToken[];
+  footer?: HeaderFooterToken[];
+  transferInfo: {
+    conditions: {
+      condition: CriteriaGroup | CriteriaTerminal;
+      owner?: string;
+      account?: string;
+    };
+    else: {
+      owner?: string;
+      account?: string;
+    };
+  };
 }
