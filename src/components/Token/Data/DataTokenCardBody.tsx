dc54325f5a2a19f0aa7abec3f90d179efc0dbce4
Noah Overcash <novercash@ebsco.com>
2023-05-21 23:41:57 -0500

    Add infrastructure

---
+++ src/components/Token/Data/DataTokenCardBody.tsx
@@ -0,0 +1,53 @@
+import { Row } from '@folio/stripes/components';
+import React, { useMemo } from 'react';
+import { useField } from 'react-final-form';
+import { DataTokenType } from '../../../types/TokenTypes';
+import ArbitraryTextToken from '../Shared/ArbitraryTextToken';
+import CurrentDateToken from '../Shared/CurrentDateToken';
+import WhitespaceToken from '../Shared/WhitespaceToken';
+
+export const EMPTY_BODY_TYPES = [
+  DataTokenType.COMMA,
+  DataTokenType.NEWLINE,
+  DataTokenType.NEWLINE_MICROSOFT,
+  DataTokenType.TAB,
+];
+
+export function isDataBodyEmpty(type: DataTokenType | undefined) {
+  return EMPTY_BODY_TYPES.includes(type ?? DataTokenType.NEWLINE);
+}
+
+export default function DataCardBody({ name }: { name: string }) {
+  const type = useField<DataTokenType>(`${name}.type`, {
+    subscription: { value: true },
+    format: (value) => value ?? DataTokenType.NEWLINE,
+  }).input.value;
+
+  const cardInterior = useMemo(() => {
+    switch (type) {
+      case DataTokenType.ARBITRARY_TEXT:
+        return (
+          <Row>
+            <ArbitraryTextToken prefix={`${name}.`} />
+          </Row>
+        );
+      case DataTokenType.SPACE:
+        return (
+          <Row>
+            <WhitespaceToken prefix={`${name}.`} />
+          </Row>
+        );
+      case DataTokenType.CURRENT_DATE:
+        return (
+          <Row>
+            <CurrentDateToken prefix={`${name}.`} />
+          </Row>
+        );
+
+      default:
+        return <div />;
+    }
+  }, [type]);
+
+  return <div>{cardInterior}</div>;
+}
---
+++ src/components/Token/Data/DataTypeSelect.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { DataTokenType } from '../../../types/TokenTypes';
+import { Select } from '@folio/stripes/components';
+import { Field } from 'react-final-form';
+
+export default function DataTypeSelect({ name }: { name: string }) {
+  return (
+    <Field name={name} defaultValue={DataTokenType.NEWLINE}>
+      {(fieldProps) => (
+        <Select<DataTokenType>
+          {...fieldProps}
+          required
+          marginBottom0
+          dataOptions={[
+            // TODO: sort these alphabetically per i18n
+            {
+              label: 'Newline (LF)',
+              value: DataTokenType.NEWLINE,
+            },
+            {
+              label: 'Newline (Microsoft, CRLF)',
+              value: DataTokenType.NEWLINE_MICROSOFT,
+            },
+            {
+              label: 'Tab',
+              value: DataTokenType.TAB,
+            },
+            {
+              label: 'Comma',
+              value: DataTokenType.COMMA,
+            },
+            {
+              label: 'Whitespace',
+              value: DataTokenType.SPACE,
+            },
+
+            {
+              label: '',
+              value: DataTokenType.NEWLINE,
+              disabled: true,
+            },
+
+            {
+              label: 'Arbitrary text',
+              value: DataTokenType.ARBITRARY_TEXT,
+            },
+            {
+              label: 'Current date',
+              value: DataTokenType.CURRENT_DATE,
+            },
+            {
+              label: 'Conditional text',
+              value: DataTokenType.CONSTANT_CONDITIONAL,
+            },
+
+            {
+              label: '',
+              value: DataTokenType.NEWLINE,
+              disabled: true,
+            },
+
+            {
+              label: 'Account date',
+              value: DataTokenType.ACCOUNT_DATE,
+            },
+            {
+              label: 'Account info',
+              value: DataTokenType.ACCOUNT_METADATA,
+            },
+            {
+              label: 'Item info',
+              value: DataTokenType.ITEM_INFO,
+            },
+            {
+              label: 'User info',
+              value: DataTokenType.USER_DATA,
+            },
+          ]}
+        />
+      )}
+    </Field>
+  );
+}
