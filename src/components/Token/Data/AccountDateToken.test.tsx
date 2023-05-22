0045acc0fe877e7a8a4ca2768c51eadca0d07302
Noah Overcash <novercash@ebsco.com>
2023-05-22 02:08:23 -0500

    Add tests for FeeFineTypeToken

--- src/components/Token/Data/AccountDateToken.test.tsx
+++ src/components/Token/Data/AccountDateToken.test.tsx
@@ -4,11 +4,7 @@ import arrayMutators from 'final-form-arrays';
 import React from 'react';
 import { Form } from 'react-final-form';
 import withIntlConfiguration from '../../../test/util/withIntlConfiguration';
-import {
-  DataTokenType,
-  HeaderFooterTokenType,
-} from '../../../types/TokenTypes';
-import HeaderFooterCardBody from '../HeaderFooter/HeaderFooterCardBody';
+import { DataTokenType } from '../../../types/TokenTypes';
 import DataCardBody from './DataTokenCardBody';
 
 describe('Account date token', () => {
---
+++ src/components/Token/Data/FeeFineTypeToken.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import arrayMutators from 'final-form-arrays';
+import React from 'react';
+import { Form } from 'react-final-form';
+import { DataTokenType } from '../../../types/TokenTypes';
+import DataCardBody from './DataTokenCardBody';
+
+describe('Fee/fine type token', () => {
+  it('displays appropriate form', async () => {
+    const submitter = jest.fn();
+
+    render(
+      <Form
+        mutators={{ ...arrayMutators }}
+        onSubmit={(v) => submitter(v)}
+        initialValues={{ test: { type: DataTokenType.FEE_FINE_TYPE } }}
+      >
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <DataCardBody name="test" />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Form>
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    expect(submitter).toHaveBeenLastCalledWith({
+      test: {
+        type: DataTokenType.FEE_FINE_TYPE,
+        attribute: 'FEE_FINE_TYPE_NAME',
+      },
+    });
+
+    await userEvent.selectOptions(
+      screen.getByRole('combobox', { name: 'Attribute' }),
+      'Type ID'
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    expect(submitter).toHaveBeenLastCalledWith({
+      test: {
+        type: DataTokenType.FEE_FINE_TYPE,
+        attribute: 'FEE_FINE_TYPE_ID',
+      },
+    });
+  });
+});
