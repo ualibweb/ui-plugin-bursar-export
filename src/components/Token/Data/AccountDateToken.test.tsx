d9b48bb4dfe208f2c80ffad56857f12e4d9f1aa6
Noah Overcash <novercash@ebsco.com>
2023-05-22 02:05:44 -0500

    Add AccountDateToken test

---
+++ src/components/Token/Data/AccountDateToken.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import arrayMutators from 'final-form-arrays';
+import React from 'react';
+import { Form } from 'react-final-form';
+import withIntlConfiguration from '../../../test/util/withIntlConfiguration';
+import {
+  DataTokenType,
+  HeaderFooterTokenType,
+} from '../../../types/TokenTypes';
+import HeaderFooterCardBody from '../HeaderFooter/HeaderFooterCardBody';
+import DataCardBody from './DataTokenCardBody';
+
+describe('Account date token', () => {
+  it('displays appropriate form', async () => {
+    const submitter = jest.fn();
+
+    render(
+      withIntlConfiguration(
+        <Form
+          mutators={{ ...arrayMutators }}
+          onSubmit={(v) => submitter(v)}
+          initialValues={{ test: { type: DataTokenType.ACCOUNT_DATE } }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <DataCardBody name="test" />
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        </Form>
+      )
+    );
+
+    await userEvent.selectOptions(
+      screen.getByRole('combobox', { name: 'Format' }),
+      'Quarter'
+    );
+    await userEvent.selectOptions(
+      screen.getByRole('combobox', { name: 'Timezone' }),
+      'UTC'
+    );
+    await userEvent.selectOptions(
+      screen.getByRole('combobox', { name: 'Date' }),
+      'Last updated date'
+    );
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Fallback value' }),
+      'placeholder'
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    expect(submitter).toHaveBeenLastCalledWith({
+      test: {
+        type: DataTokenType.ACCOUNT_DATE,
+        attribute: 'UPDATED',
+        format: 'QUARTER',
+        timezone: 'UTC',
+        placeholder: 'placeholder',
+      },
+    });
+  });
+});
