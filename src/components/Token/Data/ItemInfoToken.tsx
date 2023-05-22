071075a6ebb347381d8788e1c149163f09c5cf10
Noah Overcash <novercash@ebsco.com>
2023-05-22 01:52:13 -0500

    Extract common user/item info to resolve sonar duplications

--- src/components/Token/Data/ItemInfoToken.tsx
+++ src/components/Token/Data/ItemInfoToken.tsx
@@ -1,71 +1,42 @@
-import { Col, Select, TextField } from '@folio/stripes/components';
 import React from 'react';
-import { Field } from 'react-final-form';
 import { ItemAttribute } from '../../../types/TokenTypes';
+import UserItemInfoToken from './UserItemInfoToken';
 
 export default function ItemInfoToken({ prefix }: { prefix: string }) {
   return (
-    <>
-      <Col xs={12} md={6}>
-        <Field<ItemAttribute> name={`${prefix}attribute`} defaultValue="NAME">
-          {(fieldProps) => (
-            <Select<ItemAttribute>
-              {...fieldProps}
-              required
-              label="Value"
-              dataOptions={[
-                {
-                  label: 'Name',
-                  value: 'NAME',
-                },
-                {
-                  label: 'Barcode',
-                  value: 'BARCODE',
-                },
-                {
-                  label: 'Material type',
-                  value: 'MATERIAL_TYPE',
-                },
-                {
-                  label: 'Institution ID',
-                  value: 'INSTITUTION_ID',
-                },
-                {
-                  label: 'Campus ID',
-                  value: 'CAMPUS_ID',
-                },
-                {
-                  label: 'Library ID',
-                  value: 'LIBRARY_ID',
-                },
-                {
-                  label: 'Location ID',
-                  value: 'LOCATION_ID',
-                },
-              ]}
-            />
-          )}
-        </Field>
-      </Col>
-      <Col xs={12} md={6}>
-        <Field name={`${prefix}placeholder`}>
-          {(fieldProps) => (
-            <TextField<string>
-              {...fieldProps}
-              fullWidth
-              label="Fallback value"
-            />
-          )}
-        </Field>
-      </Col>
-      <Col xs={12}>
-        <p style={{ margin: 0 }}>
-          <i>
-            If the chosen value is not available/applicable, the fallback value
-            will be used instead.
-          </i>
-        </p>
-      </Col>
-    </>
+    <UserItemInfoToken<ItemAttribute>
+      prefix={prefix}
+      defaultValue="NAME"
+      options={[
+        {
+          label: 'Name',
+          value: 'NAME',
+        },
+        {
+          label: 'Barcode',
+          value: 'BARCODE',
+        },
+        {
+          label: 'Material type',
+          value: 'MATERIAL_TYPE',
+        },
+        {
+          label: 'Institution ID',
+          value: 'INSTITUTION_ID',
+        },
+        {
+          label: 'Campus ID',
+          value: 'CAMPUS_ID',
+        },
+        {
+          label: 'Library ID',
+          value: 'LIBRARY_ID',
+        },
+        {
+          label: 'Location ID',
+          value: 'LOCATION_ID',
+        },
+      ]}
+    />
   );
 }
--- src/components/Token/Data/UserInfoToken.tsx
+++ src/components/Token/Data/UserInfoToken.tsx
@@ -1,78 +1,46 @@
-import { Col, Select, TextField } from '@folio/stripes/components';
 import React from 'react';
-import { Field } from 'react-final-form';
 import { UserAttribute } from '../../../types/TokenTypes';
+import UserItemInfoToken from './UserItemInfoToken';
 
 export default function UserInfoToken({ prefix }: { prefix: string }) {
   return (
-    <>
-      <Col xs={12} md={6}>
-        <Field<UserAttribute>
-          name={`${prefix}attribute`}
-          defaultValue="EXTERNAL_SYSTEM_ID"
-        >
-          {(fieldProps) => (
-            <Select<UserAttribute>
-              {...fieldProps}
-              required
-              label="Value"
-              dataOptions={[
-                {
-                  label: 'Folio ID',
-                  value: 'FOLIO_ID',
-                },
-                {
-                  label: 'External system ID',
-                  value: 'EXTERNAL_SYSTEM_ID',
-                },
-                {
-                  label: 'Patron group ID',
-                  value: 'PATRON_GROUP_ID',
-                },
-                {
-                  label: 'Barcode',
-                  value: 'BARCODE',
-                },
-                {
-                  label: 'Username',
-                  value: 'USERNAME',
-                },
-                {
-                  label: 'First name',
-                  value: 'FIRST_NAME',
-                },
-                {
-                  label: 'Middle name',
-                  value: 'MIDDLE_NAME',
-                },
-                {
-                  label: 'Last name',
-                  value: 'LAST_NAME',
-                },
-              ]}
-            />
-          )}
-        </Field>
-      </Col>
-      <Col xs={12} md={6}>
-        <Field name={`${prefix}placeholder`}>
-          {(fieldProps) => (
-            <TextField<string>
-              {...fieldProps}
-              fullWidth
-              label="Fallback value"
-            />
-          )}
-        </Field>
-      </Col>
-      <Col xs={12}>
-        <p style={{ margin: 0 }}>
-          <i>
-            If the chosen value is not available/applicable, the fallback value
-            will be used instead.
-          </i>
-        </p>
-      </Col>
-    </>
+    <UserItemInfoToken<UserAttribute>
+      prefix={prefix}
+      defaultValue="EXTERNAL_SYSTEM_ID"
+      options={[
+        {
+          label: 'Folio ID',
+          value: 'FOLIO_ID',
+        },
+        {
+          label: 'External system ID',
+          value: 'EXTERNAL_SYSTEM_ID',
+        },
+        {
+          label: 'Patron group ID',
+          value: 'PATRON_GROUP_ID',
+        },
+        {
+          label: 'Barcode',
+          value: 'BARCODE',
+        },
+        {
+          label: 'Username',
+          value: 'USERNAME',
+        },
+        {
+          label: 'First name',
+          value: 'FIRST_NAME',
+        },
+        {
+          label: 'Middle name',
+          value: 'MIDDLE_NAME',
+        },
+        {
+          label: 'Last name',
+          value: 'LAST_NAME',
+        },
+      ]}
+    />
   );
 }
