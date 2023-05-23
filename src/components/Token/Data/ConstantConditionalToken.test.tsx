8a08a7b7c8d3933516466f6e58943caf66a7ff16
Noah Overcash <novercash@ebsco.com>
2023-05-23 03:15:45 -0500

    Update ConstantConditionalToken tests

--- src/components/Token/Data/ConstantConditionalToken.test.tsx
+++ src/components/Token/Data/ConstantConditionalToken.test.tsx
@@ -13,80 +13,122 @@ import { DataTokenType } from '../../../types/TokenTypes';
 import DataTokenCardBody from './DataTokenCardBody';
 
 describe('Constant conditional token', () => {
-  describe('buttons work as expected', () => {
+  it('add works as expected', async () => {
     const submitter = jest.fn();
 
-    beforeEach(() => {
-      render(
-        withIntlConfiguration(
-          <Form
-            mutators={{ ...arrayMutators }}
-            onSubmit={(v) => submitter(v)}
-            initialValues={{
-              test: {
-                type: DataTokenType.CONSTANT_CONDITIONAL,
-                conditions: [
-                  {
-                    type: CriteriaTerminalType.AGE,
-                    numDays: '10',
-                    value: 'if value 1',
-                  },
-                  {
-                    type: CriteriaTerminalType.AMOUNT,
-                    operator: ComparisonOperator.GREATER_THAN,
-                    amountDollars: '20',
-                    value: 'if value 2',
-                  },
-                ],
-                else: 'fallback else',
-              },
-            }}
-          >
-            {({ handleSubmit }) => (
-              <form onSubmit={handleSubmit}>
-                <DataTokenCardBody name="test" />
-                <button type="submit">Submit</button>
-              </form>
-            )}
-          </Form>
-        )
-      );
+    render(
+      withIntlConfiguration(
+        <Form
+          mutators={{ ...arrayMutators }}
+          onSubmit={(v) => submitter(v)}
+          initialValues={{
+            test: {
+              type: DataTokenType.CONSTANT_CONDITIONAL,
+              conditions: [
+                {
+                  type: CriteriaTerminalType.AGE,
+                  numDays: '10',
+                  value: 'if value 1',
+                },
+                {
+                  type: CriteriaTerminalType.AMOUNT,
+                  operator: ComparisonOperator.GREATER_THAN,
+                  amountDollars: '20',
+                  value: 'if value 2',
+                },
+              ],
+              else: 'fallback else',
+            },
+          }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <DataTokenCardBody name="test" />
+              <button type="submit">Submit</button>
+            </form>
+          )}
+        </Form>
+      )
+    );
+
+    await userEvent.click(
+      screen.getByRole('button', { name: 'Add condition' })
+    );
+    await userEvent.type(
+      screen.getAllByRole('textbox', { name: 'Then use:' })[2],
+      'if value 3'
+    );
+
+    await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+
+    expect(submitter).toHaveBeenLastCalledWith({
+      test: {
+        type: DataTokenType.CONSTANT_CONDITIONAL,
+        conditions: [
+          {
+            type: CriteriaTerminalType.AGE,
+            numDays: '10',
+            value: 'if value 1',
+          },
+          {
+            type: CriteriaTerminalType.AMOUNT,
+            operator: ComparisonOperator.GREATER_THAN,
+            amountDollars: '20',
+            value: 'if value 2',
+          },
+          {
+            type: CriteriaGroupType.ALL_OF,
+            value: 'if value 3',
+          },
+        ],
+        else: 'fallback else',
+      },
     });
+  });
 
-    it('add works as expected', async () => {
-      await userEvent.click(
-        screen.getByRole('button', { name: 'Add condition' })
-      );
-      await userEvent.type(
-        screen.getAllByRole('textbox', { name: 'Then use:' })[2],
-        'if value 3'
-      );
+  describe('aggregate is respected', () => {
+    test.each([
+      [undefined, true],
+      [true, false],
+      [false, true],
+    ])(
+      'if aggregate=%s then item/etc options should be present=%s',
+      (aggregate, shouldHaveNonAggregateCriteria) => {
+        render(
+          withIntlConfiguration(
+            <Form
+              mutators={{ ...arrayMutators }}
+              onSubmit={jest.fn()}
+              initialValues={{
+                aggregate,
+                test: {
+                  type: DataTokenType.CONSTANT_CONDITIONAL,
+                  conditions: [
+                    {
+                      type: CriteriaTerminalType.AGE,
+                      numDays: '10',
+                      value: 'if value 1',
+                    },
+                  ],
+                  else: 'fallback else',
+                },
+              }}
+            >
+              {() => <DataTokenCardBody name="test" />}
+            </Form>
+          )
+        );
 
-      await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
-
-      expect(submitter).toHaveBeenLastCalledWith({
-        test: {
-          type: DataTokenType.CONSTANT_CONDITIONAL,
-          conditions: [
-            {
-              type: CriteriaTerminalType.AGE,
-              numDays: '10',
-              value: 'if value 1',
-            },
-            {
-              type: CriteriaTerminalType.AMOUNT,
-              operator: ComparisonOperator.GREATER_THAN,
-              amountDollars: '20',
-              value: 'if value 2',
-            },
-            {
-              type: CriteriaGroupType.ALL_OF,
-              value: 'if value 3',
-            },
-          ],
-          else: 'fallback else',
-        },
-      });
-    });
+        if (shouldHaveNonAggregateCriteria) {
+          expect(
+            screen.getByRole('option', { name: 'Item location' })
+          ).toBeInTheDocument();
+        } else {
+          expect(
+            screen.queryByRole('option', { name: 'Item location' })
+          ).toBeNull();
+        }
+      }
+    );
   });
 });
