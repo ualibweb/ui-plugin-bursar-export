84a847f46988ca88d9ab36070b987d99dd73093a
Noah Overcash <novercash@ebsco.com>
2023-05-24 13:52:15 -0500

    Test filter conversion

--- src/api/dto/criteriaToFilterDto.ts
+++ src/api/dto/criteriaToFilterDto.ts
@@ -21,7 +21,7 @@ export default function criteriaToFilterDto(
       return {
         type: 'Amount',
         condition: criteria.operator ?? 'LESS_THAN_EQUAL',
-        amount: guardNumber(criteria.amountDollars, 0),
+        amount: guardNumber(criteria.amountDollars, 0) * 100,
       };
 
     case CriteriaTerminalType.FEE_FINE_TYPE:
