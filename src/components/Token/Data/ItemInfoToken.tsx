7acf86e91b1c1e57ae00ea672d64231a201cc60c
nhanaa <nhanpro7d@gmail.com>
2023-06-26 13:57:18 -0500

    Refactor code to fix duplications

--- src/components/Token/Data/ItemInfoToken.tsx
+++ src/components/Token/Data/ItemInfoToken.tsx
@@ -5,51 +5,43 @@ import { useIntl } from 'react-intl';
 
 export default function ItemInfoToken({ prefix }: { prefix: string }) {
   const intl = useIntl();
+  const attributeOptions = [
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.name',
+      value: 'NAME',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.barcode',
+      value: 'BARCODE',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.material',
+      value: 'MATERIAL_TYPE',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.instId',
+      value: 'INSTITUTION_ID',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.campId',
+      value: 'CAMPUS_ID',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.libId',
+      value: 'LIBRARY_ID',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.locId',
+      value: 'LOCATION_ID',
+    },
+  ];
   const options = useMemo(() => {
-    const selectOptions = [
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.name',
-        }),
-        value: 'NAME' as ItemAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.barcode',
-        }),
-        value: 'BARCODE' as ItemAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.material',
-        }),
-        value: 'MATERIAL_TYPE' as ItemAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.instId',
-        }),
-        value: 'INSTITUTION_ID' as ItemAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.campId',
-        }),
-        value: 'CAMPUS_ID' as ItemAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.libId',
-        }),
-        value: 'LIBRARY_ID' as ItemAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.itemInfo.locId',
-        }),
-        value: 'LOCATION_ID' as ItemAttribute,
-      },
-    ].sort((a, b) => a.label.localeCompare(b.label));
+    const selectOptions = attributeOptions
+      .map((option) => ({
+        label: intl.formatMessage({ id: option.labelId }),
+        value: option.value as ItemAttribute,
+      }))
+      .sort((a, b) => a.label.localeCompare(b.label));
     return selectOptions;
   }, [intl]);
 
--- src/components/Token/Data/UserInfoToken.tsx
+++ src/components/Token/Data/UserInfoToken.tsx
@@ -6,57 +6,46 @@ import { UserAttribute } from '../../../types/TokenTypes';
 export default function UserInfoToken({ prefix }: { prefix: string }) {
   const intl = useIntl();
 
+  const attributeOptions = [
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.folioId',
+      value: 'FOLIO_ID',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.extId',
+      value: 'EXTERNAL_SYSTEM_ID',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.groupId',
+      value: 'PATRON_GROUP_ID',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.barcode',
+      value: 'BARCODE',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.username',
+      value: 'USERNAME',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.firstname',
+      value: 'FIRST_NAME',
+    },
+    {
+      labelId:
+        'ui-plugin-bursar-export.bursarExports.token.userInfo.middlename',
+      value: 'MIDDLE_NAME',
+    },
+    {
+      labelId: 'ui-plugin-bursar-export.bursarExports.token.userInfo.lastname',
+      value: 'LAST_NAME',
+    },
+  ];
   const options = useMemo(() => {
-    const selectOptions = [
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.folioId',
-        }),
-        value: 'FOLIO_ID' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.extId',
-        }),
-        value: 'EXTERNAL_SYSTEM_ID' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.groupId',
-        }),
-        value: 'PATRON_GROUP_ID' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.barcode',
-        }),
-        value: 'BARCODE' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.username',
-        }),
-        value: 'USERNAME' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.firstname',
-        }),
-        value: 'FIRST_NAME' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.middlename',
-        }),
-        value: 'MIDDLE_NAME' as UserAttribute,
-      },
-      {
-        label: intl.formatMessage({
-          id: 'ui-plugin-bursar-export.bursarExports.token.userInfo.lastname',
-        }),
-        value: 'LAST_NAME' as UserAttribute,
-      },
-    ];
+    const selectOptions = attributeOptions.map((option) => ({
+      label: intl.formatMessage({ id: option.labelId }),
+      value: option.value as UserAttribute,
+    }));
     return selectOptions;
   }, [intl]);
 
